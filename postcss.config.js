5d681538fda1139ad7f5115558a9196227cd0fbc
Ismail Cherri <ismailcherri@gmail.com>
2021-06-27 16:44:58 +0200

    Adds PostCSS configuration

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('postcss-preset-env')({
+      stage: 1,
+    }),
+    require('postcss-nested'),
+    require('postcss-calc'),
+    require('postcss-css-variables'),
+    require('autoprefixer'),
+  ],
+};
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { store } from './app/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
