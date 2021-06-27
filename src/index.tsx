6fb28637b2293cb773bac32783fe2ebeaa3510bf
Ismail Cherri <ismailcherri@gmail.com>
2021-06-27 11:29:21 +0200

    Cleanup of create react app boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
-import { loadQuestionsAsync } from 'src/features/question/question-api';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,17 +13,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-store.dispatch(loadQuestionsAsync());
-// store.dispatch(loadQuestionAsync('/questions/4'));
-// store.dispatch(voteChoiceAsync('/questions/4/choices/32'));
-
-// store.dispatch(
-//   addQuestionAsync({
-//     question: 'Favourite pasta?',
-//     choices: ['Bucatini', 'Fettuccine', 'Spaghetti', 'Linguine', 'Pappardelle'],
-//   }),
-// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
